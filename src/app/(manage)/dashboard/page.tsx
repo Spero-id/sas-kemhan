e5cdd22809e3f6ebe9f9fd8bfbab7634659c8cab
Muhamad Rafi Prasetya <codingmuhamadrafiprasetya@gmail.com>
2025-04-14 19:16:11 +0700

    Navigation dashboard

--- src/app/(manage)/dashboard/page.tsx
+++ src/app/(manage)/dashboard/page.tsx
@@ -1,16 +1,9 @@
 "use client";
 
-import Link from "next/link";
-
 export default function Dashboard() {
   return (
-    <div className="p-4 w-full bg-white rounded">
-      <div className="flex justify-between items-center">
-        <h5 className="text-xl font-bold">Galery</h5>
-        <Link href={"/galery/create"} className="btn">
-          Create
-        </Link>
-      </div>
+    <div className="flex">
+      
     </div>
   );
 }
