aaea3a2902f1029dcc23c6b9ef901ebedb85e201
Muhamad Rafi Prasetya <codingmuhamadrafiprasetya@gmail.com>
2025-04-20 20:54:04 +0700

    UI chat

--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -2,11 +2,15 @@
 
 import { useEffect, useRef, useState } from "react";
 import { io } from "socket.io-client";
+import { MdMessage } from "react-icons/md";
+import { IoClose } from "react-icons/io5";
+import { IoSend } from "react-icons/io5";
 
 export default function ChatClient() {
   const [socket, setSocket] = useState<any>(null);
   const [messages, setMessages] = useState<any[]>([]);
   const [input, setInput] = useState("");
+  const [modalChat, setModalChat] = useState(false);
   const [page, setPage] = useState(1);
   const [hasMore, setHasMore] = useState(true);
   const containerRef = useRef<HTMLDivElement>(null);
@@ -17,13 +21,9 @@ export default function ChatClient() {
       await fetchMessages(1);
     };
     init();
-    
+
     fetch("/api/socket");
     const newSocket = io();
-
-    newSocket.on("connect", () => {
-      console.log("Connected:", newSocket.id);
-    });
 
     newSocket.on("chat:message", (msg: string) => {
       setMessages((prev) => [...prev, msg]);
@@ -32,10 +32,10 @@ export default function ChatClient() {
     setSocket(newSocket);
 
     return () => {
-        newSocket.disconnect();
+      newSocket.disconnect();
     };
   }, []);
-  
+
   const fetchMessages = async (pageToFetch: number) => {
     if (isFetchingRef.current || !hasMore) return;
     isFetchingRef.current = true;
@@ -65,26 +65,47 @@ export default function ChatClient() {
   };
 
   return (
-    <div>
-      <div
-        ref={containerRef}
-        className="border p-4 h-64 overflow-y-scroll"
-        onScroll={handleScroll}
+    <>
+      <button
+        className="border border-cyan-neon rounded-md p-3 w-fit fixed bottom-3 right-3 z-50 cursor-pointer shadow-md"
+        type="button"
+        onClick={() => setModalChat(true)}
       >
-        {messages.map((msg, i) => (
-          <div key={i}>{msg.content}</div>
-        ))}
-      </div>
+        <MdMessage className="text-cyan-neon text-2xl" />
+      </button>
 
-      <div className="flex gap-2 mt-2">
-        <input
-          className="border px-2 py-1"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          onKeyDown={(e) => e.key === "Enter" && sendMessage()}
-        />
-        <button onClick={sendMessage}>Send</button>
+      <div className={`w-96 max-w-none fixed bottom-20 right-3 m-0 bg-dark-ocean border border-cyan-neon rounded-md ${modalChat ? "block" : "hidden"}`}>
+        <div className="flex justify-between bg-deep-teal p-3 text-white rounded-t-md">
+          <h5>Chating SAS</h5>
+          <IoClose className="text-2xl cursor-pointer" onClick={() => setModalChat(false)} />
+        </div>
+
+        <div
+          ref={containerRef}
+          className="p-4 h-80 overflow-y-scroll"
+          onScroll={handleScroll}
+        >
+          {messages.map((msg, i) => (
+            <div key={i}>{msg.content}</div>
+          ))}
+        </div>
+
+        <div className="flex mt-2 p-2">
+          <input
+            className="bg-deep-teal text-white px-3 py-2 rounded-l w-full bg-opacity-50"
+            placeholder="Pesan"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            onKeyDown={(e) => e.key === "Enter" && sendMessage()}
+          />
+          <button
+            onClick={sendMessage}
+            className="text-white px-3 py-1 rounded-r bg-deep-teal bg-opacity-50"
+          >
+            <IoSend className="text-cyan-neon" />
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
