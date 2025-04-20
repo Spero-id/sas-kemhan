8aa68aeba78dc6aadc2d1fa0d37f424ff8b121f8
Muhamad Rafi Prasetya <codingmuhamadrafiprasetya@gmail.com>
2025-04-20 10:40:20 +0700

    Navigation Dashboard

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -3,39 +3,50 @@ import { TbDeviceCctvFilled } from "react-icons/tb";
 import { MdCamera, MdSensorOccupied } from "react-icons/md";
 import { CiSearch } from "react-icons/ci";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 interface NavigationProps {
   search: string;
 }
 
 const Navigation = ({ search }: NavigationProps) => {
+  const pathname = usePathname();
   return (
     <div className="flex justify-between my-4">
       <div className="flex gap-3">
         <Link
-          href="/skill"
-          className={`bg-deep-teal text-white px-4 py-2 rounded flex gap-1 items-center justify-center text-lg border-cyan-neon border`}
+          href="/"
+          className={`bg-deep-teal text-white px-4 py-2 rounded flex gap-1 items-center justify-center text-lg border ${
+            pathname === "/" ? "border-cyan-neon" : "border-transparent opacity-50"
+          }`}
         >
           <TbDeviceCctvFilled />
           CCTV
         </Link>
         <Link
-          href="/skill"
-          className={`bg-deep-teal text-white px-4 py-2 rounded flex gap-1 items-center justify-center text-lg border-transparent border opacity-50`}
+          href="/sensor-gerak"
+          className={`bg-deep-teal text-white px-4 py-2 rounded flex gap-1 items-center justify-center text-lg border ${
+            pathname.includes("sensor-gerak") ? "border-cyan-neon" : "border-transparent opacity-50"
+          }`}
         >
           <MdSensorOccupied />
           Sensor Gerak
         </Link>
         <Link
-          href="/skill"
-          className={`bg-deep-teal text-white px-4 py-2 rounded flex gap-1 items-center justify-center text-lg border-transparent border opacity-50`}
+          href="/body-worn"
+          className={`bg-deep-teal text-white px-4 py-2 rounded flex gap-1 items-center justify-center text-lg border ${
+            pathname.includes("body-worn") ? "border-cyan-neon" : "border-transparent opacity-50"
+          }`}
         >
           <MdCamera />
           Body-Worn Camera
         </Link>
       </div>
       <div className="flex gap-3">
-        <select defaultValue="Pick a color" className="select bg-deep-teal bg-opacity-50 rounded text-white">
+        <select
+          defaultValue="Pick a color"
+          className="select bg-deep-teal bg-opacity-50 rounded text-white"
+        >
           <option>Semua</option>
           <option>Crimson</option>
           <option>Amber</option>
