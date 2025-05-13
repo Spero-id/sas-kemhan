9a63045652f6cd0e55105ec0a6bf4c85e22dc734
Muhamad Rafi Prasetya <codingmuhamadrafiprasetya@gmail.com>
2025-05-13 10:43:52 +0700

    try fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,24 +2,22 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { getToken } from "next-auth/jwt";
 
-export const runtime = "edge";
-
 export async function middleware(req: NextRequest) {
-  const token = await getToken({
-    req,
-    secret: process.env.NEXTAUTH_SECRET,
-  });
+  // const token = await getToken({
+  //   req,
+  //   secret: process.env.NEXTAUTH_SECRET,
+  // });
 
-  console.log(token)
-  console.log(req.headers.get('cookie'))
-  console.log(process.env.NEXTAUTH_SECRET)
+  // console.log(token)
+  // console.log(req.headers.get('cookie'))
+  // console.log(process.env.NEXTAUTH_SECRET)
 
-  if (!token) {
-    return NextResponse.json(
-      { message: "Unauthorized" },
-      { status: 401 }
-    );
-  }
+  // if (!token) {
+  //   return NextResponse.json(
+  //     { message: "Unauthorized" },
+  //     { status: 401 }
+  //   );
+  // }
 
   return NextResponse.next();
 }
