3618acf72052618ff6b47e1840036e6aec637e1c
CodingRafi <codingmuhamadrafiprasetya.gmail.com>
2025-05-20 14:43:48 +0700

    Fix API jadi dynamic

--- src/app/api/secure/sensor-gerak/route.ts
+++ src/app/api/secure/sensor-gerak/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import { getPrismaClient } from "../../../../../lib/prisma";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   const prisma = getPrismaClient();
   try {
