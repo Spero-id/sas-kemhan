be3bd524086ed737b422e04e800b78fb73d4990b
CodingRafi <codingmuhamadrafiprasetya.gmail.com>
2025-07-10 21:10:44 +0700

    recording

--- src/app/api/secure/record/stop/route.ts
+++ src/app/api/secure/record/stop/route.ts
@@ -2,12 +2,12 @@ import { NextRequest, NextResponse } from 'next/server';
 import { stopRecording } from '@/lib/ffmpegController';
 
 export async function POST(req: NextRequest) {
-  const { pathSlug }: { pathSlug: string; } = await req.json();
+  const { pathSlug, uniqueName }: { pathSlug: string; uniqueName: string } = await req.json();
 
-  if (!pathSlug) {
+  if (!pathSlug || !uniqueName) {
     return NextResponse.json({ error: 'Missing parameters' }, { status: 400 });
   }
 
-  await stopRecording(pathSlug);
+  await stopRecording(pathSlug, uniqueName);
   return NextResponse.json({ message: 'Recording stopped' });
 }
