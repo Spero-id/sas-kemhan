e6e9813a6934a28f0196e85558532d6c49df10b4
Hajid Al Akhtar <hajidalakhtar@gmail.com>
2025-08-29 14:34:19 +0700

    fix: bodycam source when regenerate mediamtx

--- src/app/api/secure/generate-yml/route.ts
+++ src/app/api/secure/generate-yml/route.ts
@@ -62,11 +62,11 @@ export async function POST() {
     }
 
     const allDevices = [...bodyWorm, ...helmet];
-1
+
     for (const device of allDevices) {
       if (device.path_slug && device.rtsp_url) {
         config.paths[device.path_slug] = {
-          source: 'publisher',
+          source: !('need_convert' in device) || device.need_convert ? "publisher" : device.rtsp_url,
         };
       }
     }
--- src/components/Navigation/Menu.tsx
+++ src/components/Navigation/Menu.tsx
@@ -12,26 +12,24 @@ export default function MenuNavigation() {
   const { data, status } = useSession();
   const pathname = usePathname();
   const searchParams = useSearchParams();
-  
+
   // Function to preserve query parameters
   const buildHrefWithQuery = (basePath: string) => {
     const queryString = searchParams?.toString();
     return queryString ? `${basePath}?${queryString}` : basePath;
   };
-  
+
   return status !== "authenticated" ? (
     <p>Loading ..</p>
   ) : (
     <div className="flex gap-3 items-center">
-
       {hasPermission(data?.user, "dashboard.cctv.view") && (
         <Link
           href={buildHrefWithQuery("/")}
-          className={`bg-deep-teal text-white px-4 py-2 rounded flex gap-1 items-center justify-center text-lg border ${
-            pathname === "/"
-              ? "border-cyan-neon"
-              : "border-transparent opacity-50"
-          }`}
+          className={`bg-deep-teal text-white px-4 py-2 rounded flex gap-1 items-center justify-center text-lg border ${pathname === "/"
+            ? "border-cyan-neon"
+            : "border-transparent opacity-50"
+            }`}
         >
           <TbDeviceCctvFilled />
           CCTV
@@ -40,40 +38,39 @@ export default function MenuNavigation() {
       {hasPermission(data?.user, "dashboard.body_worm.view") && (
         <Link
           href={buildHrefWithQuery("/body-worm")}
-          className={`bg-deep-teal text-white px-4 py-2 rounded flex gap-1 items-center justify-center text-lg border ${
-            pathname?.includes("body-worm")
-              ? "border-cyan-neon"
-              : "border-transparent opacity-50"
-          }`}
+          className={`bg-deep-teal text-white px-4 py-2 rounded flex gap-1 items-center justify-center text-lg border ${pathname?.includes("body-worm")
+            ? "border-cyan-neon"
+            : "border-transparent opacity-50"
+            }`}
         >
           <MdCamera />
           Body-Worm Camera
         </Link>
       )}
-      {/* {hasPermission(data?.user, "dashboard.helmet.view") && (
+      {hasPermission(data?.user, "dashboard.helmet.view") && (
         <Link
           href="/helmet"
-          className={`bg-deep-teal text-white px-4 py-2 rounded flex gap-1 items-center justify-center text-lg border ${
-            pathname?.includes("helmet")
-              ? "border-cyan-neon"
-              : "border-transparent opacity-50"
-          }`}
+          className={`bg-deep-teal text-white px-4 py-2 rounded flex gap-1 items-center justify-center text-lg border ${pathname?.includes("helmet")
+            ? "border-cyan-neon"
+            : "border-transparent opacity-50"
+            }`}
         >
           <MdCamera />
           Helmet
         </Link>
       )}
-      <Link
+
+      {/* <Link
         href="#"
-        className={`bg-deep-teal text-white px-4 py-2 rounded flex gap-1 items-center justify-center text-lg border ${
-          pathname?.includes("other")
-            ? "border-cyan-neon"
-            : "border-transparent opacity-50"
-        }`}
+        className={`bg-deep-teal text-white px-4 py-2 rounded flex gap-1 items-center justify-center text-lg border ${pathname?.includes("other")
+          ? "border-cyan-neon"
+          : "border-transparent opacity-50"
+          }`}
       >
         <MdCamera />
         Other
       </Link> */}
+
     </div>
   );
 }
